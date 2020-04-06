4f2fef2d0b35f9f11a7aac74a3ec5c0fd58222c1
STONE <STONE@STONE-PC>
2020-04-06 23:51:48 +0900

    Java 파일 업로드 테스트

---
+++ src/stone_board/HelloStone.java
@@ -0,0 +1,10 @@
+package stone_board;
+
+public class HelloStone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Stone!!");
+	}
+
+}
