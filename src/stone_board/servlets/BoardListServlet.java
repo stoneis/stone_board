8ad2125a5ad0dcd8a558c7b3f9b1eb716f204ce2
STONE <STONE@STONE-PC>
2020-05-06 23:24:14 +0900

    dbcp

--- src/stone_board/servlets/BoardListServlet.java
+++ src/stone_board/servlets/BoardListServlet.java
@@ -1,41 +1,50 @@
 package stone_board.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class BoardListServlet
- */
+import stone_board.db.DBConnection;
+
 @WebServlet("/BoardListServlet")
 public class BoardListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoardListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	Connection conn = DBConnection.getConnection();
+	Statement stmt = null;
+	ResultSet rs = null;
+	
+	public BoardListServlet() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		try {
+			stmt = conn.createStatement();
+			stmt.execute("SELECT COUNT(*) AS TOTAL FROM BOARD");
+			rs = stmt.getResultSet();
+
+			while (rs.next()) {
+				response.getWriter().append("BOARD TOTAL DATA COUNT : ").append(rs.getString("TOTAL"));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
+	
 }
--- src/stone_board/servlets/BoardListServlet2.java
+++ src/stone_board/servlets/BoardListServlet2.java
@@ -26,7 +26,7 @@ public class BoardListServlet2 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("WEB.XML make Servlet / Served at: ").append(request.getContextPath());
+		
 	}
 
 	/**
